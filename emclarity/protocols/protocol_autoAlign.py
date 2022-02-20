998d3947f17b79c57082847741e461ebc16ed359
alvarox97 <alvaromedinilla97@gmail.com>
2022-02-20 13:07:33 +0100

    Parameters added (with doubts)

--- emclarity/protocols/protocol_autoAlign.py
+++ emclarity/protocols/protocol_autoAlign.py
@@ -39,7 +39,7 @@ import numpy as np
 import pwem.objects as data
 from pwem.objects import Transform
 from pyworkflow import BETA
-from pyworkflow.object import Set
+from pyworkflow.object import Set, Integer
 import pyworkflow.protocol.params as params
 import tomo.objects as tomoObj
 from imod import Plugin
@@ -69,17 +69,89 @@ class EmclarityAutoAlign(Protocol):
                       important=True,
                       label='Input set of tilt-series.')
 
-        form.addParam('times', params.IntParam,
+        form.addParam('beadDiameter', params.IntParam,
                       default=10,
-                      label='Times', important=True,
-                      help='Times the message will be printed.')
+                      important=True,
+                      label='Bead diameter',
+                      help='')
 
-        form.addParam('previousCount', params.IntParam,
-                      default=0,
+        form.addParam('autoAli_max_resolution', params.IntParam,
+                      default=18,
                       allowsNull=True,
-                      label='Previous count',
-                      help='Previous count of printed messages',
-                      allowsPointers=True)
+                      label='autoAli_max_resolution',
+                      help='Low-pass cutoff, in Å, used in alignment')
+
+        form.addParam('autoAli_min_sampling_rate', params.IntParam,
+                      default=10,
+                      allowsNull=True,
+                      label='autoAli_min_sampling_rate',
+                      help='Maximum pixel size used for alignment, in Å per pixel')
+
+        form.addParam('autoAli_max_sampling_rate', params.IntParam,
+                      default=3,
+                      allowsNull=True,
+                      label='autoAli_max_sampling_rate',
+                      help='Minimum pixel size used for alignment, in Å per pixel')
+
+        form.addParam('autoAli_iterations_per_bin', params.IntParam,
+                      default=3,
+                      allowsNull=True,
+                      label='autoAli_iterations_per_bin',
+                      help='The number of patch tracking iterations, for each bin')
+
+        form.addParam('autoAli_n_iters_no_rotation', params.IntParam,
+                      default=3,
+                      allowsNull=True,
+                      label='autoAli_n_iters_no_rotation',
+                      help='The number of patch tracking iterations, for each bin,'
+                           'before activating local alignments')
+
+        form.addParam('autoAli_patch_size_factor', params.IntParam,
+                      default=4,
+                      allowsNull=True,
+                      label='autoAli_patch_size_factor',
+                      help='Sets the size of the patches used for patch tracking.'
+                           'Making this larger will result in more patches, and more local areas in later iterations,'
+                           'but may also decrease accuracy')
+
+        form.addParam('autoAli_patch_tracking_border', params.IntParam,
+                      default=64,
+                      allowsNull=True,
+                      label='autoAli_patch_tracking_border',
+                      help='Number of pixels to trim off each edge in X and in Y')
+
+        form.addParam('autoAli_patch_tracking_border', params.IntParam,
+                      default=64,
+                      allowsNull=True,
+                      label='autoAli_patch_tracking_border',
+                      help='Number of pixels to trim off each edge in X and in Y')
+
+        form.addParam('autoAli_patch_overlap', params.FloatParam,
+                      default=0.5,
+                      allowsNull=True,
+                      label='autoAli_patch_overlap',
+                      help='Fractional overlap in X and Y between patches that are tracked by correlation.'
+                           'This influences the number of patches')
+
+        form.addParam('autoAli_max_shift_in_angstroms', params.IntParam,
+                      default=40,
+                      allowsNull=True,
+                      label='autoAli_max_shift_in_angstroms',
+                      help='Maximum shifts allowed, in Å, for the patch tracking alignment')
+
+        form.addParam('autoAli_max_shift_factor', params.IntParam,
+                      default=1,
+                      allowsNull=True,
+                      label='autoAli_max_shift_factor',
+                      help='The maximum shifts allowed are progressively reduced with the iterations i')
+
+        form.addParam('autoAli_refine_on_beads', params.BooleanParam,
+                      default=False,
+                      allowsNull=True,
+                      label='autoAli_refine_on_beads',
+                      help='Whether or not the patch tracking alignment should be refined using the gold beads.'
+                           'This refinement makes the alignment significantly slower, but can substantially '
+                           'improve the quality of the alignment')
 
     # --------------------------- STEPS functions ------------------------------
     def _insertAllSteps(self):
